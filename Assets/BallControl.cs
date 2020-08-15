4cd02482219bb4b067cc545b05ceb7da5c2b5f5b
liwax <liwaalex99@gmail.com>
2020-08-16 06:08:17 +0700

    progress1

---
+++ Assets/BallControl.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    //rigibody bola
+    private Rigidbody2D rigidBody2D;
+
+    //besarnya gaya awal mendorong bola
+    public float xInitialForce;
+    public float yInitialForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+
+        //mulai game
+        RestartGame();
+    }
+
+    void ResetBall()
+    {
+        //rest posisi 
+        transform.position = Vector2.zero;
+
+        //reset kecepatan
+        rigidBody2D.velocity = Vector2.zero;
+    }
+
+    void PushBall()
+    {
+        //tentukan nilai komponen y dari gaya dorong antara -yInitialForce dan yInitialForce
+        float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
+
+        //tentukan nilai acak antai 0 (inklusif) dan 2(ekslusif)
+        float randomDirection = Random.Range(0, 2);
+
+        //jika nilai dibawah 1 akan bergerak ke kiri jika tidak ke kanan
+        if (randomDirection < 1.0f)
+        {
+            //gunakan gaya untuk menggerakkna bola ini
+            rigidBody2D.AddForce(new Vector2(-xInitialForce, yRandomInitialForce));
+        }
+        else
+        {
+            rigidBody2D.AddForce(new Vector2(xInitialForce, yRandomInitialForce));
+        }
+    }
+    void RestartGame()
+    {
+        //kembalikan bola ke poisi semula
+        ResetBall();
+
+        //setelah 2 detik , berikan gaya ke bola
+        Invoke("PushBall", 2);
+    }
+}
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    //control ke atas
+    public KeyCode upButton = KeyCode.W;
+
+    //control bawah
+    public KeyCode downButton = KeyCode.S;
+
+    //kecepatan gerak
+    public float speed = 10.0f;
+
+    //get rigibody
+    private Rigidbody2D rigidbody2D;
+
+    //batas atas dan bawah game scene (Batas bawah menggunakan (-)
+    public float yBoundary = 9.0f;
+
+    //skor pemain
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get speed raket
+        Vector2 velocity = rigidbody2D.velocity;
+
+        //jika pemain mencet atas
+        if (Input.GetKeyDown(upButton))
+        {
+            velocity.y = speed;
+        }
+        //jika pemain mencet bawah
+        else if (Input.GetKey(downButton)) {
+            velocity.y = -speed;
+        }
+        //pemain tidak memecet apapun
+        else
+        {
+            velocity.y = 0.0f;
+        }
+
+        //kembalikan nilai velocity ke rigibody
+        rigidbody2D.velocity = velocity;
+
+        //dapatkan posisi raket
+        Vector3 position = transform.position;
+
+        //jika melewati batas atas kembalikan ke posisi batas
+        if(position.y > yBoundary)
+        {
+            position.y = yBoundary;
+        }
+
+        //jika melewati batas bawah (-yBoundary)
+        else if(position.y < -yBoundary)
+        {
+            position.y = -yBoundary;
+        }
+
+        //masukkan nilai posisi ke transform
+        transform.position = position;
+    }
+
+    public void IcrementScore()
+    {
+        score++;
+    }
+
+    //mengembalikan nilai score
+    public void ResetScore()
+    {
+        score = 0;
+    }
+
+    //mendapatkan nilai score
+    public int Score
+    {
+        get { return score; }
+    }
+}
